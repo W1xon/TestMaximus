98d09076720e897848447fdf7d759f4661b068ab
Wixon <vladimir16buzo@gmail.com>
2026-04-25 22:25:19 +0700

    Add IR structure

---
+++ Source/NFS MaxEd.NET6/Maximus/Services/Generation/IR/ScriptDoc.cs
@@ -0,0 +1,23 @@
+namespace Maximus.Services.IR;
+
+public class ScriptDoc
+{
+    private readonly List<ScriptInstrucion> _instructions;
+    public IReadOnlyList<ScriptInstrucion> Instructions => _instructions;
+    
+    public ScriptDoc()
+    {
+        _instructions = new List<ScriptInstrucion>();
+    }
+    public void AddInstruction(InstrucionType type, InstructionSection section, string path, string? subject = null, string? subField = null, string? value = null)
+    {
+        var instruction = new ScriptInstrucion(type, section)
+        {
+            Path = path,
+            Subject = subject,
+            SubField = subField,
+            Value = value
+        };
+         _instructions.Add(instruction);
+    }
+}
---
+++ Source/NFS MaxEd.NET6/Maximus/Services/Generation/IR/ScriptInstrucion.cs
@@ -0,0 +1,36 @@
+namespace Maximus.Services.IR;
+public enum InstrucionType
+{
+    Comment,
+    AddNode,
+    AddField,
+    UpdateField,
+    ChangeVault,
+}
+
+public enum InstructionSection
+{
+    NodeCreation,
+    FieldDeclaration,
+    ArrayUpdate,
+    ScalarUpdate,
+    ArrayResize,
+    ChildNode,
+    ChildEntries
+}
+public class ScriptInstrucion
+{
+    public InstrucionType Type { get; }
+    public InstructionSection Section { get; }
+    public string Scope { get; } = "gameplay";
+    public string Path { get; set; }
+    public string? Subject { get; set; }
+    public string? SubField { get; set; }
+    public string? Value { get; set; }
+    
+    public ScriptInstrucion(InstrucionType type, InstructionSection section)
+    {
+        Type = type;
+        Section = section;
+    }
+}
