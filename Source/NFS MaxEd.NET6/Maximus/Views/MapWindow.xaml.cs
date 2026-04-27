fac6054cb3d5f1ed11b401302b51297d80d437bc
Wixon <vladimir16buzo@gmail.com>
2026-04-27 21:11:03 +0700

    Update map view

--- Source/NFS MaxEd.NET6/Maximus/Views/MapWindow.xaml.cs
+++ Source/NFS MaxEd.NET6/Maximus/Views/MapWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Maximus.Views
 {
@@ -9,190 +10,106 @@ namespace Maximus.Views
         private Point lastMousePosition;
         private bool isDragging = false;
         private double currentZoom = 1.0;
-        private const double MinZoom = 0.1;
-        private const double MaxZoom = 5.0;
-        private const double ZoomStep = 0.1;
+        private const double MinZoom = 0.05;
+        private const double MaxZoom = 4.0;
+        private const double ZoomStep = 1.1; 
 
         public MapWindow()
         {
             InitializeComponent();
-            Loaded += OnLoaded; 
-            UpdateZoomLabel();
+            Loaded += (s, e) => CenterAndZoomOut();
         }
-        
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            CenterAndZoomOut();
-        }
-        
-        
-        
-        
+
         private void CenterAndZoomOut()
         {
-            currentZoom = 0.1;
+            currentZoom = 0.5; 
+            UpdateScale();
+            
+            MapScrollViewer.UpdateLayout();
+            MapScrollViewer.ScrollToHorizontalOffset((MapImage.ActualWidth * currentZoom - MapScrollViewer.ActualWidth) / 2);
+            MapScrollViewer.ScrollToVerticalOffset((MapImage.ActualHeight * currentZoom - MapScrollViewer.ActualHeight) / 2);
+        }
+
+        private void MapCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point mousePos = e.GetPosition(MapScrollViewer);
+            double zoomFactor = e.Delta > 0 ? 1.1 : 0.9;
+            ApplyZoomAtPoint(zoomFactor, mousePos);
+            e.Handled = true;
+        }
+
+        private void ApplyZoomAtPoint(double factor, Point relativePoint)
+        {
+            double oldZoom = currentZoom;
+            double newZoom = Math.Clamp(currentZoom * factor, MinZoom, MaxZoom);
+
+            if (Math.Abs(newZoom - oldZoom) < 0.001) return;
+            double absoluteX = (MapScrollViewer.HorizontalOffset + relativePoint.X) / oldZoom;
+            double absoluteY = (MapScrollViewer.VerticalOffset + relativePoint.Y) / oldZoom;
+
+            currentZoom = newZoom;
+            UpdateScale();
+
+            MapScrollViewer.UpdateLayout();
+
+            MapScrollViewer.ScrollToHorizontalOffset(absoluteX * currentZoom - relativePoint.X);
+            MapScrollViewer.ScrollToVerticalOffset(absoluteY * currentZoom - relativePoint.Y);
+        }
+
+        private void UpdateScale()
+        {
             ScaleTransform.ScaleX = currentZoom;
             ScaleTransform.ScaleY = currentZoom;
-        
-            
-            double canvasWidth = MapCanvas.ActualWidth;
-            double canvasHeight = MapCanvas.ActualHeight;
-            double imageWidth = MapImage.ActualWidth;
-            double imageHeight = MapImage.ActualHeight;
-        
-            
-            if (canvasWidth > 0 && canvasHeight > 0 && imageWidth > 0 && imageHeight > 0)
-            {
-                TranslateTransform.X = (canvasWidth - imageWidth * currentZoom) / 2;
-                TranslateTransform.Y = (canvasHeight - imageHeight * currentZoom) / 2;
-            }
-        
-            UpdateZoomLabel();
+            ZoomLabel.Text = $"Zoom: {(int)(currentZoom * 100)}%";
         }
 
-
-        
-        private void DragWindow(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.DragMove();
-            }
-        }
-
-        
-        private void Minimize_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        
         private void MapCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             isDragging = true;
-            lastMousePosition = e.GetPosition(MapCanvas);
-            MapCanvas.CaptureMouse();
-            MapCanvas.Cursor = Cursors.SizeAll;
+            lastMousePosition = e.GetPosition(MapScrollViewer);
+            ContentGrid.CaptureMouse();
+            ContentGrid.Cursor = Cursors.SizeAll;
         }
 
-        
         private void MapCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             isDragging = false;
-            MapCanvas.ReleaseMouseCapture();
-            MapCanvas.Cursor = Cursors.Arrow;
+            ContentGrid.ReleaseMouseCapture();
+            ContentGrid.Cursor = Cursors.Arrow;
         }
 
-        
         private void MapCanvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (isDragging)
             {
-                Point currentPosition = e.GetPosition(MapCanvas);
-                double offsetX = currentPosition.X - lastMousePosition.X;
-                double offsetY = currentPosition.Y - lastMousePosition.Y;
+                Point currentPosition = e.GetPosition(MapScrollViewer);
+                double deltaX = currentPosition.X - lastMousePosition.X;
+                double deltaY = currentPosition.Y - lastMousePosition.Y;
 
-                TranslateTransform.X += offsetX;
-                TranslateTransform.Y += offsetY;
+                MapScrollViewer.ScrollToHorizontalOffset(MapScrollViewer.HorizontalOffset - deltaX);
+                MapScrollViewer.ScrollToVerticalOffset(MapScrollViewer.VerticalOffset - deltaY);
 
                 lastMousePosition = currentPosition;
             }
         }
 
-        
-        private void MapCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            Point mousePos = e.GetPosition(MapImage);
-            
-            double zoomFactor = e.Delta > 0 ? 1.1 : 0.9;
-            double newZoom = currentZoom * zoomFactor;
 
-            
-            if (newZoom < MinZoom) newZoom = MinZoom;
-            if (newZoom > MaxZoom) newZoom = MaxZoom;
+        private void ZoomIn_Click(object sender, RoutedEventArgs e) => 
+            ApplyZoomAtPoint(ZoomStep, new Point(MapScrollViewer.ActualWidth / 2, MapScrollViewer.ActualHeight / 2));
 
-            if (Math.Abs(newZoom - currentZoom) > 0.001)
-            {
-                
-                double scaleChange = newZoom / currentZoom;
-                
-                TranslateTransform.X = mousePos.X - (mousePos.X - TranslateTransform.X) * scaleChange;
-                TranslateTransform.Y = mousePos.Y - (mousePos.Y - TranslateTransform.Y) * scaleChange;
+        private void ZoomOut_Click(object sender, RoutedEventArgs e) => 
+            ApplyZoomAtPoint(1 / ZoomStep, new Point(MapScrollViewer.ActualWidth / 2, MapScrollViewer.ActualHeight / 2));
 
-                currentZoom = newZoom;
-                ScaleTransform.ScaleX = currentZoom;
-                ScaleTransform.ScaleY = currentZoom;
-
-                UpdateZoomLabel();
-            }
-        }
-
-        
-        
-        private void ZoomIn_Click(object sender, RoutedEventArgs e)
-        {
-            ApplyZoom(currentZoom + ZoomStep);
-        }
-
-
-        private void ZoomOut_Click(object sender, RoutedEventArgs e)
-        {
-            ApplyZoom(currentZoom - ZoomStep);
-        }
-
-
-        private void ApplyZoom(double newZoom)
-        {
-            if (newZoom < MinZoom) newZoom = MinZoom;
-            if (newZoom > MaxZoom) newZoom = MaxZoom;
-
-            if (Math.Abs(newZoom - currentZoom) < 0.001)
-                return;
-
-            
-            Point center = new Point(MapCanvas.ActualWidth / 2, MapCanvas.ActualHeight / 2);
-
-            
-            Point imageCenter = MapImage.TransformToVisual(MapCanvas).Transform(center);
-
-            double scaleChange = newZoom / currentZoom;
-
-            
-            TranslateTransform.X = imageCenter.X - (imageCenter.X - TranslateTransform.X) * scaleChange;
-            TranslateTransform.Y = imageCenter.Y - (imageCenter.Y - TranslateTransform.Y) * scaleChange;
-
-            currentZoom = newZoom;
-            ScaleTransform.ScaleX = currentZoom;
-            ScaleTransform.ScaleY = currentZoom;
-
-            UpdateZoomLabel();
-        }
-
-
-        
         private void ResetZoom_Click(object sender, RoutedEventArgs e)
         {
             currentZoom = 1.0;
-            ScaleTransform.ScaleX = currentZoom;
-            ScaleTransform.ScaleY = currentZoom;
-            TranslateTransform.X = 0;
-            TranslateTransform.Y = 0;
-            UpdateZoomLabel();
+            UpdateScale();
+            MapScrollViewer.ScrollToHorizontalOffset(0);
+            MapScrollViewer.ScrollToVerticalOffset(0);
         }
 
-        
-        private void UpdateZoomLabel()
-        {
-            if (ZoomLabel != null)
-            {
-                ZoomLabel.Text = $"Zoom: {(int)(currentZoom * 100)}%";
-            }
-        }
+        private void DragWindow(object sender, MouseButtonEventArgs e) { if (e.LeftButton == MouseButtonState.Pressed) DragMove(); }
+        private void Close_Click(object sender, RoutedEventArgs e) => Close();
+        private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
     }
 }
